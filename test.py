6acfb7c31ab5b1cc798ffc0938a84e3a354b0c09
xyxu <965418170@qq.com>
2025-10-24 22:47:49 +0800

    兼容夜盘

--- test.py
+++ test.py
@@ -16,15 +16,28 @@ class DualMovingAverage(bt.Strategy):
         # 当前K线时间
         current_time = self.data.datetime.time()
         current_datetime = self.data.datetime.datetime()
-        print(current_datetime, '时刻bidprice: ', self.data0.bid_price[0])
+        # print(current_datetime, '时刻bidprice: ', self.data0.bid_price[0])
+        # pos = self.broker.get_account_position(self.data0._name)
+        # print(current_datetime, '可用资金: ', self.broker.getcash(), "总资金: ", self.broker.getvalue())
+        # print(self.data0._name, " 持仓: ", pos)
+
         pos = self.broker.get_account_position(self.data0._name)
-        print(current_datetime, '可用资金: ', self.broker.getcash(), "总资金: ", self.broker.getvalue())
-        print(self.data0._name, " 持仓: ", pos)
+
+        if len(pos) == 0:
+            # 空仓时开空一手
+            print(current_datetime, '开空')
+            self.broker.sell_open(self.data0._name, 1, self.data0.ask_price[0])
+        else:
+            # 多仓时平空一手
+            print(current_datetime, '平空')
+            self.broker.buy_close(self.data0._name, 1, self.data0.bid_price[0])
+
+        
 
 # 创建引擎
 cerebro = bt.Cerebro()
 # 连接天勤（请在 MyStore 中配置您的登录信息）
-store = MyStore(key='xxxxxxxxx', value='xxxxxxxxxxxxx')
+store = MyStore(key='xxxxxx', value='xxxxxxx')
 # 订阅合约（示例：上期所铜主力，请按需修改）
 data = store.getdata(instrument='SHFE.cu2512')
 # 加载经济商
--- tianqin_backtrader/broker.py
+++ tianqin_backtrader/broker.py
@@ -22,14 +22,26 @@ class MyBroker(BrokerBase):
         return self.p.store.tianqin.get_account().balance
     
     def get_account_position(self, instrument: str):
-        """获取特定标的持仓(待优化)"""
+        """
+        获取特定标的持仓
+        """
         pos = self.p.store.tianqin.get_position(instrument)
+        if pos.pos == 0:
+            return {}
         return {k: v for k, v in pos.items()}
     
     def get_account_positions(self):
         """获取所有持仓(待优化)"""
         pos = self.p.store.tianqin.get_position()
-        return {k: dict(v) for k, v in pos.items()}
+        ins = list({k: dict(v) for k, v in pos.items()}.keys())
+        positions = {}
+        for i in ins:
+            p = self.get_account_position(i)
+            if len(p) == 0:
+                continue
+            else:
+                positions[i] = p
+        return positions
     
     def get_order(self, order_id):
         return self.p.store.tianqin.get_order(order_id)
@@ -43,18 +55,57 @@ class MyBroker(BrokerBase):
             volume=size, 
             limit_price=limit_price
         )
-        return order
+        self.p.store.tianqin.wait_update()
+        return [order]
     
     def sell_close(self, instrument:str, size:int, limit_price:float=None):
-        """平多"""
-        order = self.p.store.tianqin.insert_order(
-            symbol=instrument, 
-            direction='SELL', 
-            offset='CLOSE', 
-            volume=size, 
-            limit_price=limit_price
-        )
-        return order
+        """平多(先平今再平昨)"""
+        pos = self.get_account_position(instrument)
+        if len(pos)==0:
+            print("无持仓, 无法多平仓")
+            return
+        
+        if pos['pos_long'] == 0:
+            print("无多仓, 无法平仓")
+            return
+        
+        order_1 = None
+        order_2 = None
+        pos_long_today = pos['pos_long_today']
+        pos_long_his = pos['pos_long_his']
+        
+        # 先平今, 再平昨
+        if size <= pos_long_today:
+            return [self.p.store.tianqin.insert_order(
+                            symbol=instrument, 
+                            direction='SELL', 
+                            offset='CLOSETODAY', 
+                            volume=size, 
+                            limit_price=limit_price
+                        )]
+        else:
+            if pos_long_today > 0:
+                order_1 = self.p.store.tianqin.insert_order(
+                    symbol=instrument, 
+                    direction='SELL', 
+                    offset='CLOSETODAY', 
+                    volume=pos_long_today, 
+                    limit_price=limit_price
+                )
+            order_2 = self.p.store.tianqin.insert_order(
+                symbol=instrument, 
+                direction='SELL', 
+                offset='CLOSE', 
+                volume=size if pos_long_his >= size else pos_long_his, 
+                limit_price=limit_price
+            )
+            if order_1 and order_2:
+                return [order_1, order_2]
+            if order_1 and order_2 is None:
+                return [order_1]
+            if order_1 is None and order_2:
+                return [order_2]
+
     
     def sell_open(self, instrument:str, size:int, limit_price:float=None):
         """开空"""
@@ -65,18 +116,56 @@ class MyBroker(BrokerBase):
             volume=size, 
             limit_price=limit_price
         )
-        return order
+        self.p.store.tianqin.wait_update()
+        return [order]
     
     def buy_close(self, instrument:str, size:int, limit_price:float=None):
         """平空"""
-        order = self.p.store.tianqin.insert_order(
-            symbol=instrument, 
-            direction='BUY', 
-            offset='CLOSE', 
-            volume=size, 
-            limit_price=limit_price
-        )
-        return order
+        pos = self.get_account_position(instrument)
+        if len(pos)==0:
+            print("无持仓, 无法多空仓")
+            return
+        
+        if pos['pos_short'] == 0:
+            print("无空仓, 无法平仓")
+            return
+        
+        order_1 = None
+        order_2 = None
+        pos_short_today = pos['pos_short_today']
+        pos_short_his = pos['pos_short_his']
+        
+        # 先平今, 再平昨
+        if size <= pos_short_today:
+            return [self.p.store.tianqin.insert_order(
+                            symbol=instrument, 
+                            direction='BUY', 
+                            offset='CLOSETODAY', 
+                            volume=size, 
+                            limit_price=limit_price
+                        )]
+        else:
+            if pos_short_today > 0:
+                order_1 = self.p.store.tianqin.insert_order(
+                    symbol=instrument, 
+                    direction='BUY', 
+                    offset='CLOSETODAY', 
+                    volume=pos_short_today, 
+                    limit_price=limit_price
+                )
+            order_2 = self.p.store.tianqin.insert_order(
+                symbol=instrument, 
+                direction='BUY', 
+                offset='CLOSE', 
+                volume=size if pos_short_his >= size else pos_short_his, 
+                limit_price=limit_price
+            )
+            if order_1 and order_2:
+                return [order_1, order_2]
+            if order_1 and order_2 is None:
+                return [order_1]
+            if order_1 is None and order_2:
+                return [order_2]
     
     def memory_cache(self):
         return
